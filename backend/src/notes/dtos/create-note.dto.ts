15d001d966ceb946131cc59338abe8b99d6c1ad9
Narender Kumar Saini <narender.itrustss@gmail.com>
2023-02-21 22:23:20 +0530

    added validator

--- backend/src/notes/dtos/create-note.dto.ts
+++ backend/src/notes/dtos/create-note.dto.ts
@@ -1,6 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength, MinLength, IsNotEmpty } from 'class-validator';
 
 export class CreateNoteDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(20)
+  @MaxLength(300)
   noteText: string;
 }
