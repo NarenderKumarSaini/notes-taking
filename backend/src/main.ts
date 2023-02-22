c8356bf67124b6da9f9cae80f074f42ae72677d6
Narender Kumar Saini <narender.itrustss@gmail.com>
2023-02-22 22:26:50 +0530

    main.ts updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Listining on port ${port}`)
 }
 bootstrap();
