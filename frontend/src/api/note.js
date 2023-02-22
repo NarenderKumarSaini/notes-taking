86614d526b1369dd329ad35a1c0765fae278f36c
Narender Kumar Saini <narender.itrustss@gmail.com>
2023-02-23 01:23:19 +0530

    changes

--- frontend/src/api/note.js
+++ frontend/src/api/note.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import getEnvVars from '../../environment';
 
-const { apiUrl } = getEnvVars();
+//const { apiUrl } = getEnvVars();
 const instance = axios.create({
     baseURL: "https://note-nestjs.onrender.com"
 });
