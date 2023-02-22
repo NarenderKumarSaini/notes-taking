9a5007a3ea52bff8e5d3c7917521fc519fb50641
Narender Kumar Saini <narender.itrustss@gmail.com>
2023-02-22 23:54:13 +0530

    changes

---
+++ frontend/environment.js
@@ -0,0 +1,21 @@
+/*****************************
+ * environment.js
+ * path: '/environment.js' (root of your project)
+ ******************************/
+
+import Constants from 'expo-constants';
+
+const getEnvVars = (env = Constants.manifest.releaseChannel) => {
+  // What is __DEV__ ?
+  // This variable is set to true when react-native is running in Dev mode.
+  // __DEV__ is true when run locally, but false when published.
+  if (__DEV__) {
+    return ENV.dev;
+  } else if (env === 'staging') {
+    return ENV.staging;
+  } else if (env === 'prod') {
+    return ENV.prod;
+  }
+};
+
+export default getEnvVars;
